bc0c22e7c43561bf3cbdc3625cec8e8ca2c6eafa
kasiasaif <kasia.saif@gmail.com>
2021-10-29 22:13:30 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
 
   return (
     <div className="Application">
+      <h3>Weather App</h3>
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
